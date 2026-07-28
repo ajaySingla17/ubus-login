ebed45f32c89009644609179eb3be8be93bcbe86
Ajay7006-mca <ajay7006.mca25@chitkara.edu.in>
2026-07-28 23:11:05 +0530

    f

--- src/components/PaymentCard.jsx
+++ src/components/PaymentCard.jsx
@@ -43,11 +43,11 @@ export default function PaymentCard() {
             <p>BARWALA BUS</p>
             <p>STAND</p>
             <p>ROUTE NO 117</p>
-            <p className={styles.fee}>Bus Fee: 47000</p>
+            <p className={styles.fee}>Bus Fee: 50000</p>
             <p>Due Amount:</p>
-            <p className={styles.due}>47000</p>
+            <p className={styles.due}>50000</p>
             <p>Paid Amount:</p>
-            <p className={styles.paid}>47000</p>
+            <p className={styles.paid}>50000</p>
             <p className={styles.valid}>
               Valid Upto <span>30-JUN-2027</span>
             </p>
@@ -55,9 +55,9 @@ export default function PaymentCard() {
           <div className={styles.right}>
             <p className={styles.amount}>Rs. 50000</p>
             <p className={styles.success}>success</p>
-            <p>UT49203</p>
+            <p>UT47412</p>
             <p>pay_AbcdeF123456</p>
-            <p>10-Sep-2026</p>
+            <p>10-June-2026</p>
             <p>07:16:11 AM</p>
           </div>
         </div>
